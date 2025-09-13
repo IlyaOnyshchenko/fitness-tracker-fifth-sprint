bc81b1b0881f1b785ea4dd67cd4a9ced8268ef26
ilya_onyshchenko <ilya234onishenko@yandex.ru>
2025-09-14 00:34:17 +0300

    test commit

--- internal/trainings/trainings_test.go
+++ internal/trainings/trainings_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/IlyaOnyshchenko/fitness_tracker_fifth_sprint"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
