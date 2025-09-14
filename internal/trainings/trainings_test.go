a5cbe7cfe23548355533b80e10d864185e355024
ilya_onyshchenko <ilya234onishenko@yandex.ru>
2025-09-14 16:15:18 +0300

    for review

--- internal/trainings/trainings_test.go
+++ internal/trainings/trainings_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/IlyaOnyshchenko/fitness_tracker_fifth_sprint"
+	"github.com/IlyaOnyshchenko/fitness_tracker_fifth_sprint/internal/personaldata"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
